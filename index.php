29ca5a7659414a54510e7509b5adbe14616712b9
Yohann.B <yohann.boulade@gmail.com>
2022-10-26 08:31:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,8 @@
 <?php
 
 if (!empty($_SERVER['HTTP_ACCEPT_LANGUAGE'])) {
-
-	switch (substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2)) {
-		case 'fr':
-
-			include dirname(__FILE__) . '/../../lang/lang-fr.php';
-			break;
-
-		default:
-
-			include dirname(__FILE__) . '/../../lang/lang-fr.php';
-			break;
-	}
+	$lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+	include dirname(__FILE__) . '/../../lang/lang-'.$lang.'.php';
 } else {
 	include dirname(__FILE__) . '/../../lang/lang-fr.php';
 }
